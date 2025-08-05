af1f74688f573ffb5eb2335f292b8d68794a5ae2
Steve Boel <stevie12boel@gmail.com>
2025-08-05 22:33:25 +0200

    sdf

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,15 +4,15 @@ require 'core.lazy'
 require 'core.autocmd'
 
 require("lazy").setup({
-  --require 'plugins.neotree',
-  --require 'plugins.bufferline',
-  require 'plugins.colortheme',
-  require 'plugins.haskell',
-  require 'plugins.treesitter',
-  require 'plugins.lualine',
-  require 'plugins.telescope',
-  require 'plugins.autocompletion',
-  require 'plugins.lsp',
-  require 'plugins.indent',
-  require 'plugins.toggleterminal',
+	--require 'plugins.neotree',
+	--require 'plugins.bufferline',
+	require 'plugins.colortheme',
+	require 'plugins.haskell',
+	require 'plugins.treesitter',
+	require 'plugins.lualine',
+	require 'plugins.telescope',
+	require 'plugins.autocompletion',
+	require 'plugins.lsp',
+	require 'plugins.indent',
+	require 'plugins.toggleterminal',
 })
